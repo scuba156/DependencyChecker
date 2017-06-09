a429071afbf16496961748be40fade0e02a3b50a
Steven Holman <scuba156@gmail.com>
2017-06-10 04:14:25 +1000

    Add project files.

---
+++ DependencyChecker/Source/Main.cs
@@ -0,0 +1,20 @@
+﻿using HugsLib;
+using HugsLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace $rootnamespace$
+{
+    public class Main : ModBase {
+    public Main() {
+        Instance = this;
+    }
+
+    public override string ModIdentifier { get { return "$rootnamespace$"; } }
+
+    internal static Main Instance { get; private set; }
+    internal static ModLogger Log { get { return Instance.Logger; } }
+}
+}
